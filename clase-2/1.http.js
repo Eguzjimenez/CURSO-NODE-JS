f863a5cbd6ebb5ee55bb0393736b7647333fec40
Esteban <esguzjimenez232@gmail.com>
2024-05-29 21:51:42 -0600

    https, routing y api.http

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,12 +1,30 @@
 const http = require('node:http') // protocolo HTTP
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
   if (req.url === '/') {
-    res.statusCode = 200 // ; OK
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Bienvenido a mi mpagina de inicio')
+    res.statusCode = 200 // OK
+    res.end('<h1>Bienvenido a mi página de inicio</h1>')
+  } else if (req.url === '/imagen-super-bonita.png') {
+    fs.readFile('./cyberpunk.jpg', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Server Error</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/jpg')
+        res.end(data)
+      }
+    })
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 // OK
+    res.end('<h1>Contacto</h1>')
+  } else {
+    res.statusCode = 404 // Not Found
+    res.end('<h1>404</h1>')
   }
 }
 
